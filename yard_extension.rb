c2f7aa37669886199b8a552a4f9e28ef685f22c5
ser1zw <azariahsawtikes@gmail.com>
2016-05-02 03:31:30 +0900

    fix documentation script

--- yard_extension.rb
+++ yard_extension.rb
@@ -1,12 +1,12 @@
+# -*- mode: ruby; coding: utf-8 -*-
 require 'yard'
 
 # yardoc -e yard_extension.rb ext/opencv/*.cpp
 
 module YARD
   module Parser
-    module C
-      class CParser < Base
-        alias_method :initialize_original, :initialize
+    module Cpp
+      class CppParser < YARD::Parser::C::CParser
 
         def initialize(source, file = '(stdin)')
           source = source.lines.map { |line|
@@ -14,16 +14,16 @@ module YARD
             line.gsub!(/RUBY_METHOD_FUNC\s*\((?:[0-9a-zA-Z_:]+::)([0-9a-zA-Z_]+)\)/) { "RUBY_METHOD_FUNC(#{$1})" }
             line
           }.join
-          initialize_original(source, file)
+          super(source, file)
         end
       end
     end
   end
 end
 
-# YARD::Parser::SourceParser.register_parser_type(:c, YARD::Parser::C::CParser, ['c', 'cc']) # Remove .cpp
-# YARD::Parser::SourceParser.register_parser_type(:cpp, YARD::Parser::Cpp::CppParser, ['cpp'])
+YARD::Parser::SourceParser.register_parser_type(:c, YARD::Parser::C::CParser, ['c', 'cc']) # Remove .cpp
+YARD::Parser::SourceParser.register_parser_type(:cpp, YARD::Parser::Cpp::CppParser, ['cpp'])
 # YARD::Handlers::Processor.register_handler_namespace(:cpp, YARD::Handlers::Cpp)
-# YARD::Handlers::Processor.register_handler_namespace(:cpp, YARD::Handlers::C)
-YARD::Parser::SourceParser.parser_type = :c
+YARD::Handlers::Processor.register_handler_namespace(:cpp, YARD::Handlers::C)
+YARD::Parser::SourceParser.parser_type = :cpp
 YARD::Tags::Library.define_tag('OpenCV Function', :opencv_func, :with_types_and_name)
